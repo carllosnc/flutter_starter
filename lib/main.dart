2be67fcaf1404dfce8a240fdb59ab13e49fa2255
Carlos Costa <carllos.nc@gmail.com>
2023-01-29 11:38:48 -0300

    update: remove custom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import '/app.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.black,
-      statusBarColor: Colors.transparent,
-    ),
-  );
 
   runApp(const App());
 }
