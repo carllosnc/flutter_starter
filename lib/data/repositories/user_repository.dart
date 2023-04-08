e034e52d47988ea818dca8be5628ca87af6410f1
Carlos Costa <carllos.nc@gmail.com>
2023-04-08 16:20:46 -0300

    adding repositories to data layer

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,24 @@
+import '/domain/domain.dart';
+import 'package:http/http.dart' as http;
+
+class UserRepository implements UserRepositoryContract {
+  static const String _baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  @override
+  Future<http.Response> getUsers() async {
+    var response = await http.get(
+      Uri.parse('$_baseUrl/users'),
+    );
+
+    return response;
+  }
+
+  @override
+  Future<http.Response> getUser({required num id}) async {
+    var response = await http.get(
+      Uri.parse('$_baseUrl/users/$id'),
+    );
+
+    return response;
+  }
+}
