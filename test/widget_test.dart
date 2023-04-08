9a60ba48e7308df3b18283aaf74c3efad35b333a
Carlos Costa <carllos.nc@gmail.com>
2023-04-08 16:19:52 -0300

    new test cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,27 @@
+import 'package:flutter_starter/domain/domain.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_starter/data/data.dart';
 
 void main() {
-  test('test description', () {
-    expect(true, true);
+  test('Should get all users', () async {
+    final UserRepository userRepository = UserRepository();
+    final UserAdapter userAdapter = UserAdapter();
+
+    final response = await userRepository.getUsers();
+    final users = userAdapter.users(response.body);
+
+    expect(response.statusCode, 200);
+    expect(users.length, 10);
+  });
+
+  test('Should get one user', () async {
+    final UserAdapter userAdapter = UserAdapter();
+    final UserRepository userRepository = UserRepository();
+
+    final response = await userRepository.getUser(id: 1);
+    final user = userAdapter.user(response.body);
+
+    expect(response.statusCode, 200);
+    expect(user.id, 1);
   });
 }
