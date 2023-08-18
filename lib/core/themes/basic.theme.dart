8e37a7d25bdc442f740db5d907e0b45f533aa264
Carlos Costa <carllos.nc@gmail.com>
2023-08-18 09:02:39 -0300

    set main color

--- lib/core/themes/basic.theme.dart
+++ lib/core/themes/basic.theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 appTheme({required bool isLight}) => ThemeData(
       useMaterial3: true,
       brightness: isLight ? Brightness.light : Brightness.dark,
-      colorSchemeSeed: Colors.red,
+      colorSchemeSeed: Colors.blue,
       fontFamily: 'SFProDisplay',
     );
