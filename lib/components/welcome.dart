3fde71a90a2fc8158ef6eac1b8eefcd42d031dad
Carlos Costa <carllos.nc@gmail.com>
2023-09-14 20:04:10 -0300

    fix imports

--- lib/components/welcome.dart
+++ lib/components/welcome.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/core/core.dart';
+import '/imports/app.dart';
 
 class Welcome extends StatelessWidget {
   final Function onPressed;
---
+++ lib/imports/app.dart
@@ -0,0 +1,6 @@
+export '/core/core.dart';
+export '/components/components.dart';
+export '/pages/pages.dart';
+export '/data/data.dart';
+export '/notifiers/notifiers.dart';
+export '/domain/domain.dart';
