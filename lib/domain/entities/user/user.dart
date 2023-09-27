89bdde0b75a832cad394aa685b381c2eb37d80b8
Carlos Costa <carllos.nc@gmail.com>
2023-09-27 15:44:30 -0300

    fromJson and toJson methods

--- lib/domain/entities/user/user.dart
+++ lib/domain/entities/user/user.dart
@@ -15,4 +15,8 @@ class User {
     required this.username,
     required this.email,
   });
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
