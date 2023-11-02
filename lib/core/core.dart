74e485ca07e2d95073ef558891fb6ee1c4ff1cd3
Carlos Costa <carllos.nc@gmail.com>
2023-11-02 16:06:49 -0300

    feat: extension to lighten and darken colors

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,4 @@
 export '/core/themes/basic.theme.dart';
 export '/core/routes/router.dart';
 export '/core/shared_preferences/shared_preferences.dart';
+export '/core/extensions/color_extension.dart';
