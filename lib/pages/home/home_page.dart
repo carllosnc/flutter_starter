786e7e482dc6ab0e78afad78bb5493e0a2804196
Carlos Costa <carllos.nc@gmail.com>
2024-02-13 00:32:18 -0300

    Refactor HomePage widget

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,38 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import '/imports/app.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool firstTime = sharedPreferences.prefs.getBool('welcome') ?? true;
-
-  @override
   Widget build(BuildContext context) {
-    if (firstTime) {
-      return Welcome(
-        onPressed: () {
-          setState(() {
-            firstTime = false;
-          });
-        },
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Starter'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => context.push('/settings'),
-          ),
-        ],
       ),
       body: const Center(
         child: Text('🍎', style: TextStyle(fontSize: 50)),
