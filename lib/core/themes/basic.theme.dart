ee9ef399d92430f7b5e5cbb8be66921c0baab3bf
Carlos Costa <carllos.nc@gmail.com>
2024-02-13 00:32:08 -0300

    Refactor appTheme function and add pageTransition constants

--- lib/core/themes/basic.theme.dart
+++ lib/core/themes/basic.theme.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
-appTheme({required bool isLight}) => ThemeData(
-      useMaterial3: true,
-      brightness: isLight ? Brightness.light : Brightness.dark,
-      colorSchemeSeed: Colors.blue,
-      fontFamily: 'SFProDisplay',
-    );
+const pageTransition = PageTransitionsTheme(
+  builders: {
+    TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+    TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+  },
+);
+
+lightTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    colorSchemeSeed: Colors.blue,
+    fontFamily: 'SFProDisplay',
+    pageTransitionsTheme: pageTransition,
+  );
+}
+
+darkTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    colorSchemeSeed: Colors.blue,
+    fontFamily: 'SFProDisplay',
+    pageTransitionsTheme: pageTransition,
+  );
+}
