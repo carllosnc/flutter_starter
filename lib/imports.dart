f5c2ddac9340404d3412fbe29fde2e32914a4f63
Carlos Costa <carllos.nc@gmail.com>
2024-03-31 19:13:26 -0300

    Add import statements for new files

---
+++ lib/imports.dart
@@ -0,0 +1,19 @@
+//pages
+export 'pages/home_page.dart';
+export 'pages/welcome_page.dart';
+
+//core
+export './core/themes/basic.theme.dart';
+export './core/routes/router.dart';
+export './core/shared_preferences/shared_preferences.dart';
+export './core/extensions/color_extension.dart';
+export './core/constants/assets.dart';
+
+//globals
+export './globals/theme_global.dart';
+
+//data
+export './data/repositories/repository_base.dart';
+
+//domain
+export './domain/user/user.dart';
