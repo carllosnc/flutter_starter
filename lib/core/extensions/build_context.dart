7464641c08d9a8ee43bc0e106936aa58d9643f0b
Carlos Costa <carllos.nc@gmail.com>
2024-09-09 10:56:21 -0300

    Moving snackbar to components folder

--- lib/core/extensions/build_context.dart
+++ lib/core/extensions/build_context.dart
@@ -7,20 +7,3 @@ extension AppColorsContext on BuildContext {
 extension AppAssetsContext on BuildContext {
   String get welcomentBackgroundImage => "assets/images/welcome.png";
 }
-
-extension AppActionsContext on BuildContext {
-  void showSnackbar({
-    required String message,
-    SnackBarAction? action,
-    Color? backgroundColor,
-  }) {
-    ScaffoldMessenger.of(this).clearSnackBars();
-    ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(
-        backgroundColor: backgroundColor,
-        action: action,
-        content: Text(message),
-      ),
-    );
-  }
-}
