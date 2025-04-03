dec696f8b4feacf0286c8653e2102a2752354c74
Carlos Costa <carllos.nc@gmail.com>
2025-04-03 05:19:16 -0300

    Initialize reactive preferences and update theme preference handling in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:reactive_preferences/rp_container.dart';
 import '/app.dart';
 import '/imports.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //load shared preferences
-  await sharedPreferences.loadPreferences();
+  //Reactive preferences
+  await RP.init();
+  ThemePreference.init();
+  WelcomePreference.init();
 
   runApp(const App());
 }
