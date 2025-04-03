ab75e48e5afe54821aa3264d5b6529f70d4313f1
Carlos Costa <carllos.nc@gmail.com>
2025-04-03 05:19:13 -0300

    Update imports to include counter_state and theme preferences

--- lib/imports.dart
+++ lib/imports.dart
@@ -5,13 +5,12 @@ export './features/settings_page.dart';
 
 //core
 export './core/routes/router.dart';
-export './core/externals/shared_preferences.dart';
 export './core/extensions/color_extension.dart';
 export './core/extensions/build_context.dart';
 
 //shared states
 export './shared_states/shared_states.dart';
-export './shared_states/theme_state.dart';
+export 'shared_states/counter_state.dart';
 
 //data
 export './data/repositories/repository_base.dart';
@@ -25,3 +24,7 @@ export './components/button.dart';
 export './components/snackbar.dart';
 export './components/gap.dart';
 export './components/size.dart';
+
+//preferences
+export './preferences/theme_preference.dart';
+export './preferences/welcome_preference.dart';
