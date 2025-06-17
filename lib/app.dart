6543bbb8cecb66ffdd8f67099bfd2de62940cf97
Carlos Costa <carllos.nc@gmail.com>
2025-06-17 06:10:24 -0300

    Use Fabric theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:fabric/fabric_config.dart';
 import 'package:flutter/material.dart';
 import 'package:reactive_preferences/rp_mixin.dart';
 import '/imports.dart';
@@ -13,23 +14,8 @@ class _AppState extends State<App> with RPMixin {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: ThemePreference.value ? Brightness.light : Brightness.dark,
-        colorSchemeSeed: context.primaryColor,
-        fontFamily: 'SFProDisplay',
-        bottomSheetTheme: const BottomSheetThemeData(
-          surfaceTintColor: Colors.transparent,
-        ),
-        pageTransitionsTheme: const PageTransitionsTheme(
-          builders: {
-            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.windows: CupertinoPageTransitionsBuilder(),
-            TargetPlatform.fuchsia: CupertinoPageTransitionsBuilder(),
-          },
-        ),
-      ),
+      theme: ThemePreference.value ? fabricConfig.getLightTheme() : fabricConfig.getDarkTheme(),
+      darkTheme: ThemePreference.value ? fabricConfig.getDarkTheme() : fabricConfig.getLightTheme(),
       debugShowCheckedModeBanner: false,
       routerConfig: router,
     );
